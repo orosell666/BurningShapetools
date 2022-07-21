ac17644a1dc84f8dcb57810e3364936e71311bc6
orosell666 <o.rosell.ribas@gmail.com>
2022-07-21 15:11:50 +0000

    problemas con la base

--- src/api/models.py
+++ src/api/models.py
@@ -43,6 +43,13 @@ class Snowparks(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
+    user = db.relationship('User', backref='user', lazy=True, cascade = "all,delete")
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
+        nullable=False)
+    country = db.relationship('Country', backref='country', lazy=True, cascade = "all,delete")
+    companies = db.Column(db.Integer, db.ForeignKey('companies.id'),
+        nullable=False)
+    companies = db.relationship('Companies', backref='companies', lazy=True, cascade = "all,delete")
 
     def __repr__(self):
         return '<Snowparks >' %self.id
@@ -59,4 +66,46 @@ class Snowparks(db.Model):
             "bullydrivers": self.bullydrivers,
 
 
+        }
+
+class Companies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    location = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(80), nullable=False)
+
+
+    snowparks_id = db.Column(db.Integer, db.ForeignKey('snowparks.id'),
+        nullable=False)
+    snowparks = db.relationship('Snowparks', backref='snowparks', lazy=True, cascade = "all,delete")
+    
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
+        nullable=False)
+    country = db.relationship('Country', backref='country', lazy=True, cascade = "all,delete")
+    
+    
+    def serialize(self):
+
+        return {
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "country": self.country,
+        }
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+        nullable=False)
+    user = db.relationship('User', backref='user', lazy=True, cascade = "all,delete")
+
+    def serialize(self):
+
+        return {
+            "id": self.id,
+            "name": self.name,
         }
