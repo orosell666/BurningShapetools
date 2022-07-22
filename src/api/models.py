a8b360a1cffc3abc0aa7c19b069b7be5afe687a0
orosell666 <o.rosell.ribas@gmail.com>
2022-07-22 16:09:08 +0000

    error en la base

--- src/api/models.py
+++ src/api/models.py
@@ -41,15 +41,13 @@ class Snowparks(db.Model):
     bullydrivers = db.Column(db.Integer, nullable=False)
 
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
-        nullable=False)
-    user = db.relationship('User', backref='user', lazy=True, cascade = "all,delete")
+    
     country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
         nullable=False)
     country = db.relationship('Country', backref='country', lazy=True, cascade = "all,delete")
-    companies = db.Column(db.Integer, db.ForeignKey('companies.id'),
-        nullable=False)
-    companies = db.relationship('Companies', backref='companies', lazy=True, cascade = "all,delete")
+    #companies_id = db.Column(db.Integer, db.ForeignKey('companies.id'),
+       # nullable=False)
+    #companies = db.relationship('Companies', backref='companies', lazy=True, cascade = "all,delete")
 
     def __repr__(self):
         return '<Snowparks >' %self.id
@@ -72,16 +70,16 @@ class Companies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     location = db.Column(db.String(80), nullable=False)
-    country = db.Column(db.String(80), nullable=False)
+    
 
 
     snowparks_id = db.Column(db.Integer, db.ForeignKey('snowparks.id'),
         nullable=False)
     snowparks = db.relationship('Snowparks', backref='snowparks', lazy=True, cascade = "all,delete")
     
-    country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
-        nullable=False)
-    country = db.relationship('Country', backref='country', lazy=True, cascade = "all,delete")
+    #country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
+     #   nullable=False)
+    #country = db.relationship('Country', backref='country', lazy=True, cascade = "all,delete")
     
     
     def serialize(self):
@@ -102,7 +100,12 @@ class Country(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
     user = db.relationship('User', backref='user', lazy=True, cascade = "all,delete")
-
+    #snowparks_id = db.Column(db.Integer, db.ForeignKey('snowparks.id'),
+    #    nullable=False)
+    #snowparks = db.relationship('Snowparks', backref='snowparks', lazy=True, cascade = "all,delete")
+    companies_id = db.Column(db.Integer, db.ForeignKey('companies.id'),
+        nullable=False)
+    companies = db.relationship('Companies', backref='companies', lazy=True, cascade = "all,delete")
     def serialize(self):
 
         return {
