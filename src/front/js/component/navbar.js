7b5c01e6dbc037ef7489f3fb100522e4386590ee
orosell666 <o.rosell.ribas@gmail.com>
2022-08-04 13:31:35 +0000

    arreglos en front

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,11 +6,11 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Burning Shapetools</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/jobs">
+						<button className="btn btn-primary">Wanna use the shapetool this winter?</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -14,18 +14,10 @@ export const Home = () => {
 			<h1>Burning Shapetools!!</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate efficitur augue non volutpat. Morbi ultricies vitae ex vitae luctus. Praesent efficitur eleifend ex non aliquam. Curabitur sodales felis a erat lobortis, at pretium dui elementum. Curabitur porttitor ipsum quis bibendum rhoncus. In vitae dapibus ex, tristique efficitur tellus. Phasellus at dapibus magna. Vivamus sit amet nulla accumsan urna gravida scelerisque.</p>
 
-			<div className="container">
+			<div className="container mt-5">
 				<SnowparkCard />
 			</div>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+
 		</div>
 	);
 };
