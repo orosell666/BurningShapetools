b8db9869bd1ad48292c26a7cf7ec9dd94e936fb4
orosell666 <o.rosell.ribas@gmail.com>
2022-08-09 16:05:49 +0000

    carrousel arreglado

--- src/front/js/component/snowparklist.js
+++ src/front/js/component/snowparklist.js
@@ -15,23 +15,23 @@ export const SnowparkCard = (props) => {
             return (
 
 
-                < div >
-
-                    <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" >
-                        <img src="https://c.tenor.com/UH_6a9UuIMMAAAAC/cool.gif" className="card-img-top" alt="..." />
-                        <div className="card-body text-center">
-                            <h5 className="card-title">{value.name}</h5>
-                            <p className="card-text">Location: {value.location}</p>
-                            <p className="card-text">Number of shapers: {value.shapers}</p>
-                            <p className="card-text">Number of catdrivers: {value.bullydrivers}</p>
-                            <p className="card-text">Company in charge: {value.companies}</p>
-                            <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target={`#exampleModal_${value.id}`}>More Info</button>
 
 
-                            <SnowparkModal id={value.id} name={value.name} resort={value.resort} location={value.location} country={value.country} shapers={value.shapers} bullydrivers={value.bullydrivers} web={value.web} machines={value.machines} comment={value.comment} />
-                        </div>
+                <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" >
+                    <img src="https://c.tenor.com/UH_6a9UuIMMAAAAC/cool.gif" className="card-img-top" alt="..." />
+                    <div className="card-body text-center">
+                        <h5 className="card-title">{value.name}</h5>
+                        <p className="card-text">Location: {value.location}</p>
+                        <p className="card-text">Number of shapers: {value.shapers}</p>
+                        <p className="card-text">Number of catdrivers: {value.bullydrivers}</p>
+                        <p className="card-text">Company in charge: {value.companies}</p>
+                        <button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target={`#exampleModal_${value.id}`}>More Info</button>
+
+
+                        <SnowparkModal id={value.id} name={value.name} resort={value.resort} location={value.location} country={value.country} shapers={value.shapers} bullydrivers={value.bullydrivers} web={value.web} machines={value.machines} comment={value.comment} />
                     </div>
                 </div>
+
 
 
 
