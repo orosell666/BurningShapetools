c63c4569a330ca29800c50c08799e06122e407a0
orosell666 <o.rosell.ribas@gmail.com>
2022-08-09 18:22:18 +0000

    arreglos front

--- src/front/js/pages/companies.js
+++ src/front/js/pages/companies.js
@@ -4,32 +4,38 @@ import PropTypes from 'prop-types';
 
 export const Company = () => {
     const { store, actions } = useContext(Context);
+
     useEffect(() => {
         actions.LoadCompanies();
     }, [])
 
 
     return (
-        store.companies.map((value, index) => {
-            return (
-
-                <div className="container">
-                    <h1 className="container mt-5 text-center">Companies that shape a lot!</h1>
-                    <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" >
-
-                        <div className="card-body">
-                            <img src="https://i.gifer.com/ICS.gif" className="card-img-top" alt="..." />
-                            <h5 className="card-title">{value.name}</h5>
-                            <p className="card-text">Location:{ }</p>
-                            <p className="card-text">Website:{ }</p>
+        <div className="container">
+            <h1 className="container mt-5 text-center">Take a look in your Country</h1>
+            <div className="container  mt-5">
+                {
+                    store.companies.map((value, index) => {
+                        return (
 
 
+                            <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" >
 
-                        </div>
-                    </div>
-                </div >
-            )
+                                <div className="card-body text-center">
+                                    <img src="https://i.gifer.com/ICS.gif" className="card-img-top" alt="..." />
+                                    <h5 className="card-title">{value.name}</h5>
+                                    <p className="card-text">Location:{ }</p>
+                                    <p className="card-text">Website:{ }</p>
 
-        })
+
+
+                                </div>
+                            </div>
+
+                        )
+
+                    }
+                    )
+                }</div></div>
     )
 }
--- src/front/js/pages/jobs.js
+++ src/front/js/pages/jobs.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 
 
-export const Jobs = props => {
+export const Jobs = () => {
 	const { store, actions } = useContext(Context);
-	const params = useParams();
+
 
 	useEffect(() => {
-
-		actions.LoadJobs(store.jobs);
-
+		actions.LoadJobs();
 	}, [])
 
 	return (
-		<div className="container text-center">
-			<h1 className="display-4">Shape Opportunities{store.jobs[params.theid]}</h1>
-			<div className="container text-center mt-5">
+		store.jobs.map((value, index) => {
 
-				<div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded mt-5" >
-					<img src="https://i.gifer.com/8yLJ.gif" className="card-img-top img-fluid mx-auto " alt="" />
-					<div className="card-body text-center">
-						<h5 className="card-title text"><strong>Job Title{ }</strong></h5>
-						<p className="card-text">Description: { }</p>
-						<p className="card-text">Company: { }</p>
 
+			<div className="container ">
+				<h1 className="container mt-5 text-center">Shape Opportunities</h1>
+				<div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded">
+
+					<div className="card-body5" >
+						<img src="https://i.gifer.com/8yLJ.gif" className="card-img-top img-fluid mx-auto " alt="" />
+						<div className="card-body text-center">
+							<h5 className="card-title text"><strong>{value.jobtitle}</strong></h5>
+							<p className="card-text">Description: {value.description}</p>
+							<p className="card-text">Company: { }</p>
+
+						</div>
 					</div>
 				</div>
-			</div>
 
-		</div>
+			</div>
+		})
 	);
 };
 
-Jobs.propTypes = {
-	match: PropTypes.object
-};
+
