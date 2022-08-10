d691f885467d221991f29de8e86f310ab40611f6
orosell666 <o.rosell.ribas@gmail.com>
2022-08-10 14:40:18 +0000

    todo funciona ok

--- src/front/js/pages/companies.js
+++ src/front/js/pages/companies.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
-import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 export const Company = () => {
     const { store, actions } = useContext(Context);
@@ -12,30 +12,32 @@ export const Company = () => {
 
     return (
         <div className="container">
-            <h1 className="container mt-5 text-center">Take a look in your Country</h1>
+            <h1 className="container mt-5 text-center">These guys push a lot of snow!</h1>
             <div className="container  mt-5">
-                {
-                    store.companies.map((value, index) => {
-                        return (
+                <div className="card-group mt-5 ">
+                    <div className="row g-4 d-flex flex-row flex-nowrap overflow-auto">
+                        {
+                            store.companies.map((value, index) => {
+                                return (
 
 
-                            <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" >
+                                    <div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded" > <div className="card-body text-center">
+                                        <img src="https://play-lh.googleusercontent.com/i5zRnn1P9yqkJW2vLLhn_MiLBxUSZVGeGl-HudSCsUWHUxIgldN54wqL55ILuoe-qgY=w2560-h1440-rw" className="card-img-top" alt="..." />
+                                        <h5 className="card-title">{value.name}</h5>
+                                        <p className="card-text">Location:{value.location}</p>
+                                        <Link to="https://{value.web}" className="stretched-link text-dark">
+                                            <p className="card-text">{value.web}</p>
+                                        </Link>
 
-                                <div className="card-body text-center">
-                                    <img src="https://i.gifer.com/ICS.gif" className="card-img-top" alt="..." />
-                                    <h5 className="card-title">{value.name}</h5>
-                                    <p className="card-text">Location:{ }</p>
-                                    <p className="card-text">Website:{ }</p>
 
 
+                                    </div>
+                                    </div>
 
-                                </div>
-                            </div>
+                                )
 
-                        )
-
-                    }
-                    )
-                }</div></div>
+                            }
+                            )
+                        }</div></div></div></div>
     )
 }
--- src/front/js/pages/jobs.js
+++ src/front/js/pages/jobs.js
@@ -11,27 +11,37 @@ export const Jobs = () => {
 	}, [])
 
 	return (
-		store.jobs.map((value, index) => {
 
 
-			<div className="container ">
-				<h1 className="container mt-5 text-center">Shape Opportunities</h1>
-				<div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded">
 
-					<div className="card-body5" >
-						<img src="https://i.gifer.com/8yLJ.gif" className="card-img-top img-fluid mx-auto " alt="" />
-						<div className="card-body text-center">
-							<h5 className="card-title text"><strong>{value.jobtitle}</strong></h5>
-							<p className="card-text">Description: {value.description}</p>
-							<p className="card-text">Company: { }</p>
+		<div className="container ">
+			<h1 className="container mt-5 text-center">Shape Opportunities</h1>
+			<div className="container  mt-5">
+				<div className="card-group mt-5 ">
+					<div className="row g-4 d-flex flex-row flex-nowrap overflow-auto">
+						{
+							store.jobs.map((value, index) => {
+								return (
 
-						</div>
-					</div>
-				</div>
 
-			</div>
-		})
-	);
-};
+
+									<div className="card col-md-3 mb-3 me-3 shadow p-3 mb-5 bg-body rounded">
+
+										<img src="https://i.gifer.com/8yLJ.gif" className="card-img-top img-fluid mx-auto " alt="" />
+
+										<h5 className="card-title text"><strong>{value.jobtitle}</strong></h5>
+										<p className="card-text">Description: {value.description}</p>
+										<p className="card-text">Where: { }</p>
+
+
+									</div>
+
+
+								)
+							}
+							)
+						}</div></div></div></div>
+	)
+}
 
 
