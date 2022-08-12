378be4abf922d440130757efb796d2a0a0e1aae0
orosell666 <o.rosell.ribas@gmail.com>
2022-08-12 18:20:05 +0000

    more front

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-sm">
+		<nav className="navbar navbar-dark bg-dark navbar-expand-sm">
 			<div className="container">
 				<div className="row">
 					<Link to="/" style={{ "text-decoration": "none" }} >
 						<p className="navbar-brand mb-0 logotipNavbar" >B.S.</p>
 					</Link></div>
-				<button className="navbar-toggler dark " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<img className="navbar-toggler-icon fa-solid fa-square-caret-down" ></img>
+				<button className="navbar-toggler dark logocolor" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<img className="navbar-toggler-icon" ></img>
 				</button>
-				<div className="collapse navbar-collapse justify-content-end dropstart dark" id="navbarNavDarkDropdown">
+				<div className="collapse navbar-collapse justify-content-end dropstart dark logocolor" id="navbarNavDarkDropdown">
 					<ul className="navbar-nav">
 						<li className="nav-item dropdown">
 							<a className="btn btn-dark dropdown-toggle " href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/front/js/component/snowparkmodal.js
+++ src/front/js/component/snowparkmodal.js
@@ -2,27 +2,32 @@ import React from "react";
 
 
 export const SnowparkModal = (props) => {
+    const imageSize = {
+        width: "500",
+
+    }
 
     return (
 
         <div className="modal fade" id={`exampleModal_${props.id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog modal-lg">
                 <div className="modal-content">
-                    < div >
 
-                        <div className="card-body">
-                            <img src={props.image_url} className="card-img-top" width={300} height={500} alt="..." />
-                            <h5 className="card-title"><strong>{props.name}</strong></h5>
-                            <p className="card-text">{props.resort}, {props.location}, {props.country}</p>
-                            <p className="card-text" >Shapers: {props.shapers}</p>
-                            <p className="card-text">Bullydrivers: {props.bullydrivers}</p>
-                            <p className="card-text" >Web: {props.web}</p>
-                            <p className="card-text">Machines: {props.machines}</p>
-                            <p className="card-text">Company in charge: {props.companies}</p>
-                            <p className="card-text">Comment: {props.comment}</p>
-                        </div>
 
-                    </div >
+                    <div className="modal-body">
+                        <h5 className="modal-title"><strong>{props.name}</strong></h5>
+                        <img src="https://c.tenor.com/_JHkX5nop_YAAAAd/skulltalk.gif" className="image-fluid" style={imageSize} alt="..." />
+
+                        <p className="modal-text">{props.resort}, {props.location}, {props.country}</p>
+                        <p className="modal-text" >Shapers: {props.shapers}</p>
+                        <p className="modal-text">Bullydrivers: {props.bullydrivers}</p>
+                        <p className="modal-text" >Web: {props.web}</p>
+                        <p className="modal-text">Machines: {props.machines}</p>
+                        <p className="modal-text">Company in charge: {props.companies}</p>
+                        <p className="modal-text">Comment: {props.comment}</p>
+                    </div>
+
+
                 </div>
             </div>
         </div>
